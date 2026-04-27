d8a1a5ac0260ea24ce96760aa9651afa2f6c994d
Lukáš "WQON" Nox <hi@wqon.art>
2026-04-27 02:08:16 +0200

    feat: add timeout for visibility of #subscribed element

--- src/scripts/killHash.js
+++ src/scripts/killHash.js
@@ -6,6 +6,18 @@ function killHash() {
       location.pathname + location.search,
     );
   }
+
+  if (location.hash === "#subscribed") {
+    setTimeout(() => {
+      history.replaceState(
+        null,
+        document.title,
+        location.pathname + location.search,
+      );
+      document.getElementById("subscribed").style.display = "none";
+      document.querySelector(".subscribe").style.display = "block";
+    }, 6000);
+  }
 }
 
 killHash();
