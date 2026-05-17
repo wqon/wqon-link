59fcbc38e0d88bd5cd8b864e50eb0567f53e9300
Lukáš "WQON" Nox <hi@wqon.dev>
2026-05-17 13:33:15 +0200

    fix: cursor offset x and y

--- src/scripts/cursor.js
+++ src/scripts/cursor.js
@@ -1,8 +1,10 @@
 const cursor = document.querySelector(".cursor");
+const offsetX = 6;
+const offsetY = 6;
 
 document.addEventListener("mousemove", (e) => {
-  cursor.style.left = e.clientX + "px";
-  cursor.style.top = e.clientY + "px";
+  cursor.style.left = e.clientX + offsetX + "px";
+  cursor.style.top = e.clientY + offsetY + "px";
 });
 
 document.querySelectorAll("[data-cursor]").forEach((el) => {
