296168031bda99c806f5df00b989ca2851380eb1
Lukáš "WQON" Nox <hi@wqon.dev>
2026-05-21 17:01:25 +0200

    refactor: switch from global styles and scripts to scoped

--- src/scripts/loader.js
+++
@@ -1,7 +0,0 @@
-window.onload = () => {
-  document.querySelector(".loader").classList.add("loaded");
-  setTimeout(() => {
-    document.querySelector("main").classList.add("loaded-content");
-    document.querySelector(".drake").classList.add("from-left");
-  }, 1600);
-};
